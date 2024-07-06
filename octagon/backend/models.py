cb5b9be45e529856253e9bea0bcba3c39babe4f8
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2024-07-07 01:41:44 +0200

    added table

--- octagon/backend/models.py
+++ octagon/backend/models.py
@@ -33,3 +33,14 @@ class AIModel(models.Model):
     accuracy = models.FloatField(null=True)
     def __str__(self):
         return self.name
+
+class ModelEvaluation(models.Model):
+    model = models.ForeignKey(AIModel, on_delete=models.CASCADE)
+    m1 = models.FloatField()
+    m2 = models.FloatField()
+    m3 = models.FloatField()
+    m4 = models.FloatField()
+    m5 = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.model.name} - {self.accuracy}'
